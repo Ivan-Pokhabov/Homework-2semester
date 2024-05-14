c68eb6219e19a9e7c04937c88fe6a77daa86b90e
Ivan Pokhabov <vanek3372@gmail.com>
2024-05-13 23:53:32 -0300

    Feat: Add comparisson encoding with and without bwt

--- LZW/LZW/Program.cs
+++ LZW/LZW/Program.cs
@@ -2,13 +2,13 @@
 
 if (args.Length < 2 || args.Length > 3)
 {
-    Console.WriteLine("Incorrect input, please try again");
+    Console.WriteLine("Incorrect input, please try again.");
     return;
 }
 
 if (!File.Exists(args[0]))
 {
-    Console.WriteLine("File with this pass doesn't exist");
+    Console.WriteLine("File with this pass doesn't exist.");
     return;
 }
 
@@ -16,19 +16,27 @@ var withBWT = args is [_, _, "--bwt"];
 
 if (args[1] == "--c")
 {
-    double result;
+    double encodeResult;
+    double anotherEncodingVariantresult;
 
     try
     {
-        result = LZWTransformer.Encode(args[0], withBWT);
+        anotherEncodingVariantresult = LZWTransformer.Encode(args[0], !withBWT);
+        encodeResult = LZWTransformer.Encode(args[0], withBWT);
     }
     catch (ArgumentException e)
     {
-        Console.WriteLine($"Encoding failed because of {e.Message}");
+        Console.WriteLine($"Encoding failed because of {e.Message}.");
         return;
     }
 
-    Console.WriteLine($"Encoding was successfuly. Compression ratio is {result}");
+    var bwtInfluence = withBWT ? encodeResult / anotherEncodingVariantresult : anotherEncodingVariantresult / encodeResult;
+
+    Console.WriteLine($"""
+        Encoding was successfuly.
+        Compression ratio is {encodeResult}.
+        BWT influence is {bwtInfluence} (with bwt / without bwt).
+        """);
 }
 else if (args[1] == "--u")
 {
@@ -38,13 +46,13 @@ else if (args[1] == "--u")
     }
     catch (ArgumentException e)
     {
-        Console.WriteLine($"Decoding failed because of {e.Message}");
+        Console.WriteLine($"Decoding failed because of {e.Message}.");
         return;
     }
 
-    Console.WriteLine($"Decoding was successfuly");
+    Console.WriteLine($"Decoding was successfuly.");
 }
 else
 {
-    Console.WriteLine("Incorrect input, please try again");
+    Console.WriteLine("Incorrect input, please try again.");
 }
